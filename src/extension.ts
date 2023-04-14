5230cb45d90f95a86b9392cfe5c8ba92fa8a50c8
JamJomJim <thejamesewens@gmail.com>
2023-04-14 17:14:20 -0500

    functional sidebar view

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CustomSidebarViewProvider } from './customSidebarViewProvider';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-cookbook" is now active!');
+	// Console diagnostic information (console.log) and errors (console.error)
+	// Will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "vscode-extension-sidebar-html" is active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-cookbook.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const provider = new CustomSidebarViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			CustomSidebarViewProvider.viewType,
+			provider
+		)
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("cookbook.menu.view", () => {
+			const message = "Menu/Title of extension is clicked !";
+			vscode.window.showInformationMessage(message);
+		})
+	);
+
+	// Command has been defined in the package.json file
+	// Provide the implementation of the command with registerCommand
+	// CommandId parameter must match the command field in package.json
+	let openWebView = vscode.commands.registerCommand('cookbook.contracts', () => {
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-cookbook!');
+		vscode.window.showInformationMessage('Command " Sidebar View [cookbook.contracts] " called.');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(openWebView);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// this method is called when your extension is deactivated
+export function deactivate() { }
