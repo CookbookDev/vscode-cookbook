66b5264c0cd16d79ed771242bd7d686655e72911
JamJomJim <thejamesewens@gmail.com>
2023-05-10 18:51:35 -0500

    add stars

--- src/ContractCard.jsx
+++ src/ContractCard.jsx
@@ -44,20 +44,51 @@ export const ContractCard = ({ contract, vscode }) => {
           >
             {contract.name}
           </h6>
-          <p
-            className="card-text"
+          <div
             style={{
-              fontSize: "10px",
               display: "flex",
+              justifyContent: "space-between",
               alignItems: "center",
-              gap: "5px",
-              marginBottom: "20px",
               marginTop: "10px",
             }}
           >
-            {contract.picture && <img src={contract.picture} width={15} height={15} style={{ borderRadius: "10px" }} />}
-            <Truncate>{contract.author}</Truncate>
-          </p>
+            <a
+              onClick={(e) => {
+                track("VScode: open cookbook", { contract: contract.address });
+                e.stopPropagation();
+              }}
+              href={`https://www.cookbook.dev/projects/${contract.author}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="card-text"
+              style={{
+                fontSize: "10px",
+                display: "flex",
+                alignItems: "center",
+                gap: "5px",
+                marginTop: "5px",
+                textDecorationColor: "var(--vscode-input-foreground)",
+              }}
+            >
+              {contract.picture && (
+                <img src={contract.picture} width={15} height={15} style={{ borderRadius: "10px" }} />
+              )}
+              <Truncate>{contract.author}</Truncate>
+            </a>
+            <Stars>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="15"
+                viewBox="0 0 17 17"
+                version="1.1"
+                width="15"
+                fill="currentColor"
+              >
+                <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 "></path>
+              </svg>
+              {contract.total || 0}
+            </Stars>
+          </div>
 
           <p className="card-text " style={{ marginTop: "20px", marginBottom: "20px", opacity: 0.8 }}>
             {contract.simpleDescription}
@@ -90,11 +121,12 @@ export const ContractCard = ({ contract, vscode }) => {
 };
 
 const Truncate = styled.div`
-  width: 70%;
+  width: 100%;
+  color: var(--vscode-input-foreground);
+  text-decoration-color: var(--vscode-input-foreground);
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  color: "var(--vscode-input-foreground)";
 `;
 
 const Card = styled.div`
@@ -102,7 +134,7 @@ const Card = styled.div`
   cursor: pointer;
   background-color: var(--vscode-input-background);
   &:hover {
-    background-color: var(--vscode-button-secondaryHoverBackground);
+    background-color: var(--vscode-button-Background);
   }
 `;
 
@@ -114,4 +146,18 @@ const DocLink = styled.a`
     color: var(--vscode-input-foreground);
     opacity: 1;
   }
+`;
+
+const Stars = styled.div`
+  width: max-content;
+  display: flex;
+  justify-content: center;
+  font-size: 8px;
+  gap: 2px;
+  align-items: center;
+  cursor: pointer;
+  text-decoration: none;
+  border-radius: 5px;
+  color: var(--vscode-gitDecoration-addedResourceForeground);
+  border: solid 1px rgb(0, 0, 0, 0);
 `;
